cc0925875790c71db80c58377ca40b5da4ee6893
Bruno Duarte <bruno.duarte@aluno.uece.br>
2025-11-22 20:00:41 -0300

    feat: Add Transactions collection to Category entity

--- src/MoneyTracker.Domain/Entities/Category.cs
+++ src/MoneyTracker.Domain/Entities/Category.cs
@@ -5,6 +5,8 @@ namespace MoneyTracker.Domain.Entities
     public Guid Id { get; private set; }
     public string Name { get; private set; }
 
+    public ICollection<Transaction> Transactions { get; private set; }
+
     private Category() { }
 
     public Category(Guid id, string name)
@@ -12,6 +14,7 @@ namespace MoneyTracker.Domain.Entities
       Id = id == Guid.Empty ? Guid.NewGuid() : id;
       if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name is required", nameof(name));
       Name = name;
+      Transactions = [];
     }
 
     public void Update(string name)
